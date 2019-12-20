c69e3fbfda49d01fb6d497aed1e8c4bc090ef582
rcrogers <1903806+rcrogers@users.noreply.github.com>
2019-12-20 18:24:27 -0500

    Fix attribution

--- yarns/synced_lfo.h
+++ yarns/synced_lfo.h
@@ -1,6 +1,6 @@
 // Copyright 2013 Emilie Gillet.
 //
-// Author: Chris Rogers (teukros@gmail.com)
+// Author: Emilie Gillet (emilie.o.gillet@gmail.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
